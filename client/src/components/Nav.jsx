389d5e339833117dc05279664815af1aaf7838f6
Faryal Ansari <faryalansari@live.com>
2021-03-08 17:23:24 -0500

    add functional links

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 function Nav() {
   return (
-    <div>
-      <h1>Nav</h1>
-    </div>
+    <header>
+      <nav>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/about">About</NavLink>
+        <NavLink to="/projects">Projects</NavLink>
+        <NavLink to="/contact">Contact</NavLink>
+      </nav>
+    </header>
   )
 }
 
