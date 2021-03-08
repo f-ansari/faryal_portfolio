30e874cadd10fb5807041c8bc931f864786af276
Faryal Ansari <faryalansari@live.com>
2021-03-08 17:11:10 -0500

    set routes'

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,24 @@
 import './App.css'
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
+import Introduction from './components/Introduction'
+import About from './components/About'
+import Projects from './components/Projects'
+import Contact from './components/Contact'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header"></header>
+
+      <main>
+        <Switch>
+          <Route exact path="/" component={Introduction} />
+          <Route path="/about" component={About} />
+          <Route path="/projects" component={Projects} />
+          <Route path="/contact" component={Contact} />
+        </Switch>
+      </main>
     </div>
   )
 }
