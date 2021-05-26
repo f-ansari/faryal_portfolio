ca2116838dcb81d403f15a4b4ac88812876b4e84
Faryal Ansari <faryalansari@live.com>
2021-05-25 23:00:06 -0400

    removed un-used code

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
-import './style/App.css'
+import './style/App.scss'
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 import Nav from './components/Nav'
-import Introduction from './pages/Introduction'
+import Home from './pages/Home'
 import About from './pages/About'
 import Projects from './pages/Projects'
 import Contact from './pages/Contact'
@@ -10,18 +10,17 @@ import Contact from './pages/Contact'
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
-
       <Nav />
 
       <main>
         <Switch>
-          <Route exact path="/" component={Introduction} />
+          <Route exact path="/" component={Home} />
           <Route path="/about" component={About} />
           <Route path="/projects" component={Projects} />
           <Route path="/contact" component={Contact} />
         </Switch>
       </main>
+      <section className="nav-bar"></section>
     </div>
   )
 }
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+      <p>It's like you are going "far" with the southern "y'all"</p>
+      <h1>Faryal Ansari</h1>
+    </div>
+  )
+}
+
+export default Home
