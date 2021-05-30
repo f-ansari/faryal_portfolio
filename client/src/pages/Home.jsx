0b7f62563cb4c1ef2ed14fea90eea2c0bde722aa
Faryal Ansari <faryalansari@live.com>
2021-05-30 01:18:46 -0400

    [pages => home] add intro card. [component => nav] (mobile) fixed hamburger icon style.

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import profilePic from '../assets/faryal_profile_pic.jpg'
 
 function Home() {
   return (
-    <div>
-      <p>It's like you are going "far" with the southern "y'all"</p>
-      <h1>Faryal Ansari</h1>
+    <div className="home-banner">
+      <img className="profile-pic" src={profilePic} alt="Faryal's profile pic"></img>
+      <div className="intro-card">
+        <h1>Faryal Ansari</h1>
+        <p>It's like you are going "far" with the southern "y'all"</p>
+      </div>
     </div>
   )
 }
