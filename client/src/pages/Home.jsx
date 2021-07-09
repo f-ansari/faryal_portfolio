964d3fdb4e65cde8f9641111416e2bdbf4272e35
Faryal Ansari <faryalansari@live.com>
2021-07-08 23:58:56 -0400

    [Home] in the middle of changing home intro card

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -6,12 +6,21 @@ import Skills from '../components/Skills'
 function Home(props) {
   return (
     <div>
-      <div className="home-banner">
+      {/* <div className="home-banner">
         <img className="profile-pic" src={profilePic} alt="Faryal's profile pic"></img>
         <div className="intro-card">
           <h1>Faryal Ansari</h1>
           <p>It's like you are going "far" with the southern "y'all"</p>
         </div>
+      </div> */}
+
+      <div className="home-content">
+        <div className="home-text">
+          <h3>it's like you are going "far" with the southern "yall"</h3>
+          <h1>Faryal Ansari</h1>
+          <h2>Let me help you make your dream app to a reality</h2>
+        </div>
+        <img className="profile-pic" src={profilePic} alt="Faryal's profile pic"></img>
       </div>
 
       <div>
