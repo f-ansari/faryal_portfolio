1cf348a86e14fae1c42b2e826b4e6bd97f067a8d
Faryal Ansari <faryalansari@live.com>
2021-07-11 06:00:05 -0400

    [Assets] Create project json to test data

--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import '../style/Project.scss'
 
 function Projects() {
   return (
-    <div className="page-title">  
-      <h1>Projects</h1>
+    <div className="project-container">
+      <div className="page-title">  
+        <h1>Projects</h1>
+      </div>
+
+      <div className="project-content-container">
+        
+      </div>
     </div>
   )
 }
