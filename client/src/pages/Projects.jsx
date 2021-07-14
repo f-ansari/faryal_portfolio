b3ac5bcc1b4b0e9bc888612a3237fb329d2e4016
Faryal Ansari <faryalansari@live.com>
2021-07-13 22:51:51 -0400

    [Projects] json file complete & and cards made

--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import '../style/Project.scss'
+import projects from '../assets/projects.json'
 
-function Projects() {
+function Projects(props) {
+  console.log(projects)
+  console.log(props)
   return (
     <div className="project-container">
       <div className="page-title">  
@@ -9,7 +12,19 @@ function Projects() {
       </div>
 
       <div className="project-content-container">
-        
+          {/* <h4>{projects[0].name}</h4>
+          <img src={projects[0].image} alt="" /> */}
+          {
+            projects.map((project) => {
+              return (
+                <div className="project-box">
+                  <h4>{project.name}</h4>
+                  <img width="300vw" src={project.image} alt={project.name} />
+                  <h3>Description: </h3><p>{project.description}</p>
+                </div>
+              )
+            })
+          }
       </div>
     </div>
   )
